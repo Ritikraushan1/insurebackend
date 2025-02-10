5d11202d6fb682bd1df2963a59a1001f5140982a
Ritik Raushan <94674369+Ritikraushan1@users.noreply.github.com>
2025-02-10 16:05:19 +0530

    Update redisClient.js

--- config/redisClient.js
+++ config/redisClient.js
@@ -1,8 +1,14 @@
 const Redis = require('ioredis');
- 
-const redisClient  = new Redis()
- 
-// client.on('connect', () => {
-//     console.log('Connected to Redis...');
-// });
-module.exports  = redisClient;
+
+// Corrected Redis URL without any extra encoding
+const redisClient = new Redis('redis://default:qtsbqxgaB17FLWhj9tBMndYYynVY8yrm@redis-12591.c276.us-east-1-2.ec2.redns.redis-cloud.com:12591');
+
+redisClient.on('connect', () => {
+    console.log('Connected to Redis...');
+});
+
+redisClient.on('error', (err) => {
+    console.log('Redis error:', err);
+});
+
+module.exports = redisClient;
